9a46e21510818a4ffb629132c3ac3101b06586d7
Azunna-Christopher <st20341138@outlook.cardiffmet.ac.uk>
2026-01-14 18:42:23 +0100

    Update app.py
    Changed the App page Icon

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import joblib
 # -------------------------------------------
 st.set_page_config(
     page_title="EcoVis | Air Quality Forecaster",
-    page_icon="📊",
+    page_icon="🌿",
     layout="wide",
     initial_sidebar_state="expanded"
 )
